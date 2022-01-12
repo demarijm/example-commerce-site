c0bc5cee2e97094418c47e6047262e30fb1f2c8d
demarijm <demarijmiller@gmail.com>
2022-01-12 03:39:26 -0500

    Products?

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -73,7 +73,7 @@ const ProductCard: FC<Props> = ({
             {!noNameTag && (
               <div className={s.header}>
                 <h3 className={s.name}>
-                  <span>{product.name}</span>
+                  <span>{product.name }</span>
                 </h3>
                 <div className={s.price}>
                   {`${price} ${product.price?.currencyCode}`}
