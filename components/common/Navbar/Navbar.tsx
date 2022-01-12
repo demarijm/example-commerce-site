4eb409828a3e91fc8dae6e794a5385cbbd44a540
demarijm <demarijmiller@gmail.com>
2022-01-12 04:09:43 -0500

    fixed some mobile things

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -43,11 +43,7 @@ const Navbar: FC<NavbarProps> = ({ links }) => (
           <UserNav />
         </div>
       </div>
-      {process.env.COMMERCE_SEARCH_ENABLED && (
-        <div className="flex pb-4 lg:px-6 lg:hidden">
-          <Searchbar id="mobile-search" />
-        </div>
-      )}
+ 
     </Container>
   </NavbarRoot>
 )
